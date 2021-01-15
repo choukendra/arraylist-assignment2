727a86079eead94a45106721bc21e54829a83081
choukendra <55104616+choukendra@users.noreply.github.com>
2021-01-15 13:15:07 -0800

    Add files via upload

--- ArrayList Assignment2/Question2.java
+++ ArrayList Assignment2/Question2.java
@@ -1,3 +1,15 @@
+/*
+even number greater than two could be written as the sum of two primes. 
+Write a method that takes an even int as a parameter. 
+It then prints to the terminal a pair of prime numbers that sum to that int. 
+If there are more than one combination that sums to the number, you just need to print ONE possible combination.
+
+	4 = 2 + 2
+	6 = 3 + 3
+	10 = 3 + 7 OR 5 + 5
+	120 = 7 + 113 OR 11 + 109 OR 13 + 107 OR … 59 + 61 (many possible answers, just print one) 
+*/
+
 import java.util.ArrayList;
 class Question1{
     public static ArrayList<Integer> primes(int n) {
@@ -5,7 +17,7 @@ class Question1{
         for(int i = 2; i <= n; i++){
             primes.add(i); 
         }
-        
+                
         for(int i = 0; i < primes.size(); i++){
             int p = primes.get(i);
             for(int j = primes.size()-1; j >= i + 1; j--){
@@ -14,13 +26,17 @@ class Question1{
                 }
             }
         }
+        
+        for(){
+            
+        }
         return primes; 
     }
 
     public static void main (String[] args) {
         ArrayList<Integer> nums = new ArrayList<Integer>(100);
 
-        System.out.println("List of prime numbers: " + primes(100));
+        System.out.println(primes(100));
     }
 
 }
